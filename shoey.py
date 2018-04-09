15fd9c9a38f4168d2d81eca6d34d5c67a295be10
Colin <colin.gilboy@gmail.com>
2018-04-09 01:31:49 +0000

    Laugh n wiggle.

--- shoey.py
+++ shoey.py
@@ -9,17 +9,70 @@ pwm.setPWMFreq(60)
 # Max found around 700
 servoMin = 150
 servoMax = 700
+servoMid = servoMin + (servoMax - servoMin) / 2
+
+debug = True
+
+mouthChannel = 0
+headChannel = 4
+
+def log(string):
+  if debug:
+    print string
+  return
+
+def setServoValue(channel, value):
+  pwm.setPWM(channel, 0, value)
+  log("Setting channel " + str(channel) + " to " + str(value))
+  return
+  
+def setMouthOpen(o = True):
+  if o:
+    log("Opening mouth")
+    openValue = servoMin + 120
+    setServoValue(mouthChannel, openValue)
+  else:
+    log("Closing mouth")
+    closedValue = servoMin
+    setServoValue(mouthChannel, closedValue)
+  return
 
 
-def 
+W_LEFT = 0
+W_CENTER = 1
+W_RIGHT = 2
+def wiggleInDirection(direction = W_CENTER):
+  log("Wiggling " + str(direction))
+  if direction == W_LEFT:
+    setServoValue(headChannel, servoMid - 70)
+  elif direction == W_CENTER:
+    setServoValue(headChannel, servoMid)
+  elif direction == W_RIGHT:
+    setServoValue(headChannel, servoMid + 70)
+  return
+
+def laugh(count = 3):
+  for x in range(0, count):
+    setMouthOpen(True)
+    time.sleep(0.25)
+    setMouthOpen(False)
+    time.sleep(0.25)
+
+  wiggleInDirection(W_LEFT)
+  time.sleep(0.15)
+  wiggleInDirection(W_RIGHT)
+  time.sleep(0.15)
+  wiggleInDirection(W_LEFT)
+  time.sleep(0.15)
+  wiggleInDirection(W_CENTER)
+  return
 
 print "For shoeth!"
 
+wiggleInDirection(W_CENTER)
+
 while (True):
-  # Change speed of continuous servo on channel O
-  pwm.setPWM(0, 0, servoMin)
-  time.sleep(3)
-  pwm.setPWM(0, 0, servoMax)
+  laugh()
   time.sleep(3)
   print "Looping!"
 
