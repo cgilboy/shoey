1d515572379b83c3f0cc7c12bb17dcb5ecc945c6
Colin <colin.gilboy@gmail.com>
2018-05-01 20:26:55 +0000

    Audio system has proper buffer sizing, things work mostly.

--- audio.py
+++ audio.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python
 from pyo import *
 from shoey import openMouthPercent
+from math import log10
 
-s = Server().boot()
+s = Server(buffersize=2048).boot()
 s.start()
 path = "./hedberg-peace.inp"
 print "path: " + path
 sf = SfPlayer(path, loop=False, mul=.4).out()
+#d = SDelay(sf, delay=0.1).out()
 fol = Follower(sf, freq=30)
 
-def snapshot():
-    val = fol.get()
-#    print "amplitude: %.2f" % (val)
-    openMouthPercent(val)
+# smaller minDB -> lower overall values. not quite logical, probably indicates bug
+minDB = 30
 
-pat = Pattern(snapshot, time=.01).play()
+def dec2dbScale(dec):
+    return max(0, (minDB + 10*log10(dec)) / minDB)
+
+def snapshot():
+    val = dec2dbScale(fol.get())
+    print "amplitude: %.2f" % (val)
+    openMouthPercent(val * 1.25)
+
+pat = Pattern(snapshot, time=.1).play()
 
 # Opens the server graphical interface.
 # s.gui(locals())
--- start-audio.sh
+++ start-audio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=/home/pi/local/bin:${PATH}
 export LD_LIBRARY_PATH=/home/pi/local/lib
-jackd -m -p 32 -d dummy &
+jackd -m -p 2048 -d dummy &
 sleep 1
 alsa_out -q1 2>&1 > /dev/null &
 sleep 1
